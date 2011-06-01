630c1f6508d7af2fc16b4f9cd26a31c717bf5564
Greg Allen <me@jga.me>
2011-06-01 07:48:47 -0700

    changed str.js to a dependency for npm

--- fidel.js
+++ fidel.js
@@ -5,38 +5,6 @@
   * MIT License
   */
 
-var str = (function() {
-	var templateCache = {};
-	var formatRegEx = /\{([^}]+)\}/g;
-
-	return {
-		format: function(s, args) {
-			return s.replace(formatRegEx, function(_, match){ return args[match]; }); 
-		}, 
-		template: function tmpl(template, data) {
-			var fn = !/\W/.test(template) ?
-			  templateCache[template] = templateCache[template] ||
-				tmpl(template) :
-			  new Function("obj",
-				"var p=[],print=function(){p.push.apply(p,arguments);};" +
-				"with(obj){p.push('" +
-				template
-				  .replace(/[\r\t\n]/g, "")
-				  .split("{!").join("\t")
-				  .replace(/((^|!})[^\t]*)'/g, "$1\r")
-				  .replace(/\t=(.*?)!}/g, "',$1,'")
-				  .split("\t").join("');")
-				  .split("!}").join("p.push('")
-				  .split("\r").join("\\'")
-			  + "');}return p.join('');");
-			return data ? fn( data ) : fn;
-		}
-	};
-})();
-if (typeof module !== "undefined") {
-	module.exports = str;
-}
-
 (function(obj) {
   var eventSplitter = /^(\w+)\s*(.*)$/;
       
