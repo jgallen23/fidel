81640a522b174dd533f577956ed3593bdde69c59
Antonio Laguna <antonio@potatolondon.com>
2014-01-17 15:08:10 +0000

    Moving to load-grunt-config and updating dependencies
    Fixes #4

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,112 +1,17 @@
 module.exports = function(grunt) {
-  grunt.initConfig({
-    info: grunt.file.readJSON('bower.json'),
-    meta: {
-      banner: '/*!\n'+
-              ' * <%= info.name %> - <%= info.description %>\n'+
-              ' * v<%= info.version %>\n'+
-              ' * <%= info.homepage %>\n'+
-              ' * copyright <%= info.copyright %> <%= grunt.template.today("yyyy") %>\n'+
-              ' * <%= info.license %> License\n'+
-              '*/\n'
-    },
-    jshint: {
-      main: [
-        'Gruntfile.js',
-        'bower.json',
-        'lib/**/*.js',
-        'test/*.js'
-      ]
-    },
-    concat: {
-      options: {
-        banner: '<%= meta.banner %>'
-      },
-      dist: {
-        src: [
-          'lib/fidel.js',
-          'lib/jquery.js'
-        ],
-        dest: 'dist/fidel.js'
-      }
-    },
-    uglify: {
-      options: {
-        banner: '<%= meta.banner %>'
-      },
-      dist: {
-        src: 'dist/fidel.js',
-        dest: 'dist/fidel.min.js'
-      }
-    },
-    watch: {
-      main: {
-        files: '<%= jshint.main %>',
-        tasks: 'scripts' 
-      },
-      ci: {
-        files: [
-          'Gruntfile.js',
-          'test/index.html'
-        ],
-        tasks: ['default']
-      }
-    },
-    mocha: {
-      all: {
-        src: 'test/index.html',
-        options: {
-          run: true,
-          growl: true,
-          reporter: 'Spec'
-        }
-      }
-    },
-    plato: {
-      main: {
-        files: {
-          'reports': ['lib/*.js']
-        }
-      }
-    },
-    reloadr: {
-      main: [
-        'example/*',
-        'dist/*'
-      ]
-    },
-    connect: {
-      server:{
-        port: 8000,
-        base: '.'
-      },
-      plato: {
-        port: 8000,
-        base: 'reports',
-        options: {
-          keepalive: true
-        }
-      },
-    },
-    bytesize: {
-      scripts: {
-        src: [
-          'dist/*'
-        ]
+
+  require('load-grunt-config')(grunt, {
+    config: {
+      info: grunt.file.readJSON('bower.json'),
+      meta: {
+        banner: '/*!\n'+
+            ' * <%= info.name %> - <%= info.description %>\n'+
+            ' * v<%= info.version %>\n'+
+            ' * <%= info.homepage %>\n'+
+            ' * copyright <%= info.copyright %> <%= grunt.template.today("yyyy") %>\n'+
+            ' * <%= info.license %> License\n'+
+            '*/\n'
       }
     }
   });
-  grunt.loadNpmTasks('grunt-contrib-jshint');
-  grunt.loadNpmTasks('grunt-contrib-concat');
-  grunt.loadNpmTasks('grunt-contrib-watch');
-  grunt.loadNpmTasks('grunt-contrib-uglify');
-  grunt.loadNpmTasks('grunt-contrib-connect');
-  grunt.loadNpmTasks('grunt-bytesize');
-  grunt.loadNpmTasks('grunt-mocha');
-  grunt.loadNpmTasks('grunt-reloadr');
-  grunt.loadNpmTasks('grunt-plato');
-  grunt.registerTask('scripts', ['jshint', 'concat', 'uglify', 'mocha', 'bytesize']);
-  grunt.registerTask('default', ['scripts']);
-  grunt.registerTask('dev', ['connect:server', 'reloadr', 'watch:main']);
-  grunt.registerTask('reports', ['plato', 'connect:plato']);
 };
