09c4674fe34998390e84acea1fefcbfa6930ec8d
Antonio Laguna <antonio@potatolondon.com>
2014-01-23 11:56:21 +0000

    Initial bits of support for fatjs
    #3

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,17 +1,8 @@
 module.exports = function(grunt) {
-
-  require('load-grunt-config')(grunt, {
-    config: {
-      info: grunt.file.readJSON('bower.json'),
-      meta: {
-        banner: '/*!\n'+
-            ' * <%= info.name %> - <%= info.description %>\n'+
-            ' * v<%= info.version %>\n'+
-            ' * <%= info.homepage %>\n'+
-            ' * copyright <%= info.copyright %> <%= grunt.template.today("yyyy") %>\n'+
-            ' * <%= info.license %> License\n'+
-            '*/\n'
-      }
-    }
+  require('fatjs')(grunt, {
+    name: 'fidel',
+    bowerExclude: [
+      'jquery'
+    ]
   });
 };
